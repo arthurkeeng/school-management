87965a49ffafadec4def325152a4c066c5381564
keeng arthur <121835767+arthurkeeng@users.noreply.github.com>
2024-12-12 13:47:02 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,44 @@
+import Announcements from "@/components/Announcements"
+import AttendanceChart from "@/components/AttendanceChart"
+import CountChart from "@/components/CountChart"
+import EventCalendar from "@/components/EventCalendar"
+import FinanceChart from "@/components/FinanaceChart"
+import UserCard from "@/components/UserCard"
+// this would be the school's homepage
 
-import Link from "next/link";
-
-const HomePage =()=>{
+const AdminPage =()=>{
     return <div
     className="p-4 flex gap-4 flex-col md:flex-row"
     >
-       <div className="p-6 space-y-4">
-      This would have the school's default homepage
-    </div>
-        <Link  href = "/admin" className= 'btn p-3 absolute top-4 left-5'>
-            <button>Click here for more
-            </button>
-        </Link>
+        {/* the left */}
+        <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* user cards */}
+        <div className="flex gap-4 justify-between flex-wrap">
+            <UserCard type = "student"/>
+            <UserCard type = "teacher"/>
+            <UserCard type = "admin"/>
+            <UserCard type = "parent"/>
+        </div>
+        {/* middle charts */}
+        <div className=" flex gap-4 flex-col lg:flex-row">
+            {/* count chart */}
+            <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart/>
+            </div>
+            {/* attendance chart */}
+            <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart/></div>
+        </div>
+        {/* bottom charts */}
+        <div className="w-full h-[500px]">
+            <FinanceChart/>
+        </div>
+        </div>
+        {/* the right */}
+        <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar/>
+        <Announcements/>
+        </div>
     </div>
 }
-export default HomePage
+export default AdminPage
